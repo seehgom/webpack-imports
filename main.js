c72b964c8a314d3c16d3d5ed204dea04f236d8fa
Moghees <electricinfodata@gmail.com>
2023-10-03 14:21:59 -0700

    code for sync imports

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
+import {onkeyup} from "./interactivity/sum";
+
 const inputElement = document.getElementById('sum_input');
 const resultsElement = document.getElementById('results');
 
-inputElement.onfocus = async () => {
-	const {onkeyup} = await import('./interactivity/sum');
-	inputElement.onkeyup = onkeyup(resultsElement);
-}
+inputElement.onkeyup = onkeyup(resultsElement);
